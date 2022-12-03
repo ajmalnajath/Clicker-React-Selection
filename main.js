a001ec95ce2ccc2664a50a0a4ddab888d0182585
Ajmal Najath <43416826+ajmalnajath@users.noreply.github.com>
2022-12-04 00:26:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let increment = document.getElementById("increment");
+let decrement = document.getElementById("decrement");
+
+let int = document.getElementById("counter");
+let integer = 0;
+
+increment.addEventListener('click', function() {
+    integer += 1;
+    int.innerHTML = integer;
+})
+
+decrement.addEventListener('click', function(){
+    integer -= 1;
+    int.innerHTML = integer;
+})
